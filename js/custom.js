0536fee58ad5b0c872fd5767440f9996235d60d1
Hussain01mo <hussain01mo@gmail.com>
2025-10-14 13:53:39 -0400

    Hussain first portfolio

---
+++ js/custom.js
@@ -0,0 +1,103 @@
+
+// new code added here
+
+
+(function ($) {
+
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000); // set duration in brackets    
+    });
+
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+          } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+          }
+    });
+
+
+    
+    $('.home-slider').owlCarousel({
+      animateOut: 'fadeOut',
+      items:1,
+      loop:true,
+      dots:false,
+      autoplayHoverPause: false,
+      autoplay: false,
+      smartSpeed: 1000,
+    })
+
+    $('.owl-courses').owlCarousel({
+      animateOut: 'fadeOut',
+      loop: true,
+      autoplayHoverPause: false,
+      autoplay: false,
+      smartSpeed: 1000,
+      dots: false,
+      nav:true,
+      navText: [
+          '<i class="fa fa-angle-left"></i>',
+          '<i class="fa fa-angle-right"></i>'
+      ],
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        1000: {
+          items: 3,
+        }
+      }
+    });
+
+    
+    $('.owl-client').owlCarousel({
+      items: 3, 
+      loop: false,
+      autoplay: false,
+      autoplayHoverPause: false,
+      smartSpeed: 0, 
+      mouseDrag: false,
+      touchDrag: false,
+      pullDrag: false, 
+      freeDrag: false,
+      dots: false, 
+      nav: false,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+          mouseDrag: false,
+          touchDrag: false
+        },
+        1000: {
+          items: 3,
+          mouseDrag: false,
+          touchDrag: false
+        }
+      }
+    });
+
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.custom-navbar a, #home a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    });  
+
+})(jQuery);
